96bba88d5394f892ec388f354d0c79021acfc559
Jabiii1 <jayveegarcia018@gmail.com>
2025-04-23 17:49:30 +0800

    a

--- CreateAccount.py
+++ CreateAccount.py
@@ -1,13 +1,14 @@
 from customtkinter import *
 
 accWindow = CTk()
-accWindow.geometry("420x600")
+accWindow.geometry("420x400")
 accWindow.title("Create an Account")
 accWindow.resizable(False, False)
 set_appearance_mode("light")
 
-title = CTkLabel(master=accWindow, text="Create your Account!",font=("Roboto", 24, "bold"))
+title = CTkLabel(master=accWindow, text="Create your Account!",font=("Roboto", 24, "bold"), text_color="#031927")
 title.pack(pady=10)
 
+Fname = CTkEntry
 
 accWindow.mainloop()
